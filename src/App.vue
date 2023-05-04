96fc81b1ae1175a0fe2927eb3db1fd575e5be1d7
smm1030 <408816223@qq.com>
2023-05-04 16:50:00 +0800

    docs(app.vue): add beian

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,11 @@
       <router-view></router-view>
     </el-config-provider>
   </div>
+  <p class="beian">
+    <a class="beian" href="https://beian.miit.gov.cn" target="_blank"
+      >浙ICP备2023011383号-1</a
+    >
+  </p>
 </template>
 <script setup lang="ts">
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
@@ -13,5 +18,16 @@ import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
   width: 100vw;
   height: 100vh;
   /* background-color: skyblue; */
+}
+.beian {
+  -webkit-text-size-adjust: 100%;
+  font: 400 16px/26px 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-family: 'SF Pro SC', 'SF Pro Text', 'SF Pro Icons', 'PingFang SC',
+    'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
+  text-align: center;
+  background: 0 0;
+  color: #0593d3;
+  text-decoration: none;
+  cursor: pointer;
 }
 </style>
